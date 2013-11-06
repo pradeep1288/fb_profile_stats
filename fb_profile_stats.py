5535aecd4b98c382b5e04ef018996e62d36e17e7
Pradeep Nayak <pradeep1288@gmail.com>
2013-11-05 22:57:34 -0800

    making it oop oriented

--- fb_profile_stats.py
+++ fb_profile_stats.py
@@ -1,33 +1,41 @@
 from facepy import GraphAPI
 
 
-OAUTH_TOKEN = 'CAACEdEose0cBAEdJqi0bzZCGlfrTNWZCqWUrbvZBoj3PE81cVojE60p8VSNFm2iSBYZBg9QAGZCLNRnAcLl4VBeQ45QRpZCLfQ9lekiMmHZCdbgqBF5auKoCURcSo0bjMkTfnAVbJAsrLMkhTSyvZBEZC6ZA7I0f1HZBysuQS7kbkxK4pVfgQrFSHSt5qjF2OEpSSKlYXDfCWZAJxwZDZD'
+OAUTH_TOKEN = 'CAACEdEose0cBADlYYN4H9tMZCMXLAmHkC1dYamrhDZAyPOCnWgWFsQYm5IiKjEwAf6pc7OcnRcWG9phihrnmTh0zTZBbsXnpQxxZASlMzx4IfpuHrZCM6ZAavGiZA04X77s5mRE2ZAdGzXM1JQdjjzBqBt8qcuk1B2chNCGZADRkq5VAZAmyxmlzjvbyF5mLxDh10FlvFkR2dgZAQZDZD'
 
 graph = GraphAPI(OAUTH_TOKEN)
 
-def get_number_of_friends(graph):
-    '''
+class FBStat:
+    def __init__(self, graph):
+        self.graph = graph
+
+    def get_number_of_friends(self):
+        '''
         This function will return the number of friends of the current user
-    '''
-    FQL = 'SELECT friend_count FROM user WHERE uid = me()'
-    return graph.fql(FQL)['data'][0]['friend_count']
+        '''
+        FQL = 'SELECT friend_count FROM user WHERE uid = me()'
+        return self.graph.fql(FQL)['data'][0]['friend_count']
 
-def get_number_male_female_friends(graph):
-    '''
+    def get_number_male_female_friends(self):
+        '''
         This function will return the number of male and female friends of the current user
-    '''
-    
+        '''
 
 
-def get_number_of_wall_posts(graph):
-    '''
+
+    def get_number_of_wall_posts(self):
+        '''
         This function will return the number of wall posts made by the current user
-    '''
-    FQL = 'SELECT wall_count FROM user WHERE uid = me()'
-    return graph.fql(FQL)['data'][0]['wall_count']
+        '''
+        FQL = 'SELECT wall_count FROM user WHERE uid = me()'
+        return self.graph.fql(FQL)['data'][0]['wall_count']
 
 #print get_number_of_friends(graph)
 #next_list = graph.get('me/friends?fields=gender')['paging']['next'].replace('https://graph.facebook.com/','')
 #friends = graph.get('me/friends',page=True)
 #for friend in friends:
 #    print len(friend['data'])
+obj = FBStat(graph)
+print obj.get_number_of_friends()
+
+
