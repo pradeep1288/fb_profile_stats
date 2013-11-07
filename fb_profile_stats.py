151e60b0357f1dd4a47e91eecbeed4c84973a0ac
Pradeep Nayak <pradeep1288@gmail.com>
2013-11-06 19:59:49 -0800

    adding the return friends based on gender

--- fb_profile_stats.py
+++ fb_profile_stats.py
@@ -1,7 +1,7 @@
 from facepy import GraphAPI
 
 
-OAUTH_TOKEN = 'CAACEdEose0cBADlYYN4H9tMZCMXLAmHkC1dYamrhDZAyPOCnWgWFsQYm5IiKjEwAf6pc7OcnRcWG9phihrnmTh0zTZBbsXnpQxxZASlMzx4IfpuHrZCM6ZAavGiZA04X77s5mRE2ZAdGzXM1JQdjjzBqBt8qcuk1B2chNCGZADRkq5VAZAmyxmlzjvbyF5mLxDh10FlvFkR2dgZAQZDZD'
+OAUTH_TOKEN = 'CAACEdEose0cBACM2DiKggpUOVTj9syXe6K1lqRkU3ZCwWEFIEZBa2VCS82fUc6X7iWNBDMrYvGYSCO9ZBlO9qq6jzWz2HnKVEWLGqhaumLZBmBLMSng3KxfZAlNLFKhx4RSO7YMpx6G2dQxQZAOnF9CR8j2YroZBzyujVihTKgCq6CVZAqLmzqxPWcOaxaBcLiNRZC5EJ799FRAZDZD'
 
 graph = GraphAPI(OAUTH_TOKEN)
 
@@ -21,6 +21,24 @@ class FBStat:
         This function will return the number of male and female friends of the current user
         '''
 
+        no_of_friends_by_gender = {}
+        no_of_friends_by_gender['male'] = 0
+        no_of_friends_by_gender['female'] = 0
+        no_of_friends_by_gender['unspecified'] = 0
+        friends = self.graph.get('me/friends?fields=gender', page=True)
+        for friend_page in friends:
+            for single_friend in friend_page['data']:
+                if single_friend.has_key('gender'):
+                    if single_friend['gender'] == 'male':
+                        no_of_friends_by_gender['male'] += 1
+                    elif single_friend['gender'] == 'female':
+                        no_of_friends_by_gender['female'] += 1
+                    else:
+                        no_of_friends_by_gender['unspecified'] += 1
+                else:
+                        no_of_friends_by_gender['unspecified'] += 1
+
+        return no_of_friends_by_gender
 
 
     def get_number_of_wall_posts(self):
@@ -30,12 +48,9 @@ class FBStat:
         FQL = 'SELECT wall_count FROM user WHERE uid = me()'
         return self.graph.fql(FQL)['data'][0]['wall_count']
 
-#print get_number_of_friends(graph)
-#next_list = graph.get('me/friends?fields=gender')['paging']['next'].replace('https://graph.facebook.com/','')
-#friends = graph.get('me/friends',page=True)
-#for friend in friends:
-#    print len(friend['data'])
+
 obj = FBStat(graph)
 print obj.get_number_of_friends()
+print obj.get_number_male_female_friends()
 
 
